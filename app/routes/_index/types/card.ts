5280082bca1e8336c11c1a666c6c117e9911b5cf
jennifer-fintlabs <jennifer@fintlabs.no>
2024-04-03 15:44:09 +0200

    Added routing 🐰

---
+++ app/routes/_index/types/card.ts
@@ -0,0 +1,7 @@
+import {ILink} from "~/routes/_index/types/link";
+
+export interface ICard {
+	value: string;
+	content: string;
+	links?: ILink[];
+}
---
+++ app/routes/integrations/route.tsx
@@ -0,0 +1,50 @@
+import {Box, Button, Heading, HelpText, HStack} from "@navikt/ds-react";
+import {useTranslation} from "react-i18next";
+
+// export const loader = async () => {
+//     try {
+//         const data = IntegrationApi.fetchMockData();
+//         console.log("data in route:",data)
+//         return json({ data });
+//     } catch (error) {
+//         throw new Error("Error fetching data");
+//     }
+// };
+
+export default function Index() {
+    // const loaderData = useLoaderData<typeof loader>();
+    const {t} = useTranslation('translations', {keyPrefix: 'pages.integrations'})
+
+    return (
+            <Box
+                // background="surface-alt-4-moderate"
+                padding="8"
+                paddingBlock="16"
+            >
+
+                <HStack id={'instances-custom-header'} align={"center"} justify={"space-between"} gap={"2"} wrap={false}>
+                    <HStack align={"center"} gap={"2"}>
+                        <Heading size={"medium"}>{t('header')}</Heading>
+                        <HelpText title={"Hva er dette"} placement="bottom">
+                            {t('help.header')}
+                        </HelpText>
+                    </HStack>
+
+                </HStack>
+
+                <Box
+                    id={"integration-table-container"}
+                    background={"surface-default"}
+                    padding="6"
+                    borderRadius={"large"}
+                    borderWidth="2"
+                    borderColor={"border-subtle"}
+                >
+
+                    <Heading size={"medium"}>hello world!</Heading>
+
+                </Box>
+
+            </Box>
+    );
+}
