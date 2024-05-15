1e8cd34ad5c8afa8a0b075576af365784ba80a4f
Sindre <sindre10.kodehode@gmail.com>
2024-05-15 10:14:49 +0200

    Initializing Branch

--- app/routes/flows/openObjectNode.tsx
+++ app/routes/flows/openObjectNode.tsx
@@ -1,33 +1,35 @@
 import { Handle, Position } from "reactflow";
 
-const sourceHandle1 = { top: 20 };
-const sourceHandle2 = { top: 60 };
-const sourceHandle3 = { top: 90 };
-const sourceHandle4 = { top: 120 };
-
-const nodeAmount = 4
-
 function openObjectNode({ data }) {
-  return (
-    <div>
+
+    const componentHeight = 100 + (10 * data.sources.length);
+    const sourceHeight = (componentHeight / data.sources.length) / 2 ;
+
+  console.log("componentHeight = ", componentHeight);
+  console.log("sourceHeight = ",sourceHeight);
+
+//  const customAmountHandles = data.sources.map((data.sources) =>
+//     for ( let i = 0; i > data.sources; i++ ) {
+//     <Handle type="source" position={Position.Right} style={ top: {i*sourceHeight} } />
+//     }
+// );
+return (
+  <div>
       <Handle type="target" position={Position.Left} />
-      <div className="h-[150px] w-16 flex items-center justify-center bg-slate-300 rounded-xl border border-black">
-        <img
-          src="../images/openObject.svg"
-          alt="åpne Objekt"
-          className="h-[50px]"
-        />
+      <div className="w-16 flex flex-col items-center justify-center bg-slate-300 rounded-xl border border-black" style={{ height: `${componentHeight}px` }}>
+          <img
+              src="../images/openObject.svg"
+              alt="åpne Objekt"
+              className="h-[50px]"
+          />
+          <p className="text-sm">modular</p>
       </div>
+      {data.sources.map((x, i) => (
+          <Handle key={i} type="source" position={Position.Right} style={{ top: `${(i + 1) * sourceHeight}%` }} />
+      ))}
+  </div>
+);
 
-
-
-
-
-      <Handle type="source" position={Position.Right} style={sourceHandle1} />
-      <Handle type="source" position={Position.Right} style={sourceHandle2} />
-      <Handle type="source" position={Position.Right} style={sourceHandle3} />
-      <Handle type="source" position={Position.Right} style={sourceHandle4} /> 
-    </div>
-  );
 }
+
 export default openObjectNode;
--- app/routes/flows/route.tsx
+++ app/routes/flows/route.tsx
@@ -11,10 +11,12 @@ import ReactFlow, {
 import "reactflow/dist/style.css";
 import inputNode from "./inputNode";
 import openObjectNode from "./openObjectNode";
+import openObjectNodeOld from "./openObjectOld";
 
 const nodeTypes = {
   inputNode: inputNode,
   openObject: openObjectNode,
+  openObjectOld: openObjectNodeOld,
 };
 
 const initialNodes = [
@@ -28,8 +30,39 @@ const initialNodes = [
     id: "2",
     position: { x: 300, y: 75 },
     data: { label: "2" },
+    type: "openObjectOld",
+  },
+  {
+    id: "3",
+    position: { x: 450, y: 75 },
+    data: {
+      label: "3",
+      sources: [
+        { id: "1", name: "test" },
+        { id: "2", name: "test2" },
+        { id: "3", name: "test3" },
+        { id: "4", name: "test4" },
+        { id: "5", name: "test5" },
+        { id: "6", name: "test6" },
+        { id: "7", name: "test7" },
+        { id: "8", name: "test8" },
+      ],
+    },
     type: "openObject",
   },
+  {
+    id: "4",
+    position: { x: 300, y: 250 },
+    data: {
+      label: "4",
+      sources: [
+        { id: "1", name: "test" },
+        { id: "2", name: "test2" },
+      ],
+    },
+    type: "openObject",
+  }
+
 ];
 
 const initialEdges = [
@@ -37,6 +70,13 @@ const initialEdges = [
     id: "skjema_1-2",
     source: "1",
     target: "2",
+    type: "smoothstep",
+    label: "{ } Skjema",
+  },
+  {
+    id: "skjema_1-2",
+    source: "1",
+    target: "4",
     type: "smoothstep",
     label: "{ } Skjema",
   },
