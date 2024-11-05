13c3836434d456fa8b47c1f3d41e1bd598765c16
Anum Qudsia <hello@anum.io>
2024-11-05 15:42:53 +0100

    Add DataTypeNode with List -> String

--- app/routes/integrations/nodes.tsx
+++ app/routes/integrations/nodes.tsx
@@ -1,19 +1,22 @@
 import { Node } from 'reactflow';
-import DataTypeNode from '~/routes/integrations/nodes/DataTypeNode';
-
-// Register the custom node component as a new node type
-export const nodeTypes = {
-    dataTypeNode: DataTypeNode, // Register 'dataTypeNode' as a type
-};
 
 export default [
     {
         id: '1',
-        type: 'dataTypeNode', // Use your custom node type here
+        type: 'dataTypeNode',
         position: { x: 120, y: 120 },
         data: {
             category: 'STREAM',
             elementType: { category: 'LIST', elementType: { category: 'STRING' } },
+        },
+    },
+    {
+        id: '2',
+        type: 'dataTypeNode',
+        position: { x: 120, y: 220 },
+        data: {
+            category: 'LIST',
+            elementType: { category: 'STRING' },
         },
     },
 ] as Node[];
