ce76c6d2e4d12da9cbb7b7cadb12cbc2fb011ee0
Anum Qudsia <hello@anum.io>
2025-01-03 13:34:40 +0100

    Support Variable inline node for string and integer

--- app/routes/integrations/nodes.tsx
+++ app/routes/integrations/nodes.tsx
@@ -2,74 +2,88 @@ import { Node } from 'reactflow';
 
 export default [
     // {
-    //     id: '3',
-    //     type: 'dataTypeNode',
-    //     position: { x: 120, y: 320 },
+    //     id: '1',
+    //     type: 'variableNode',
+    //     position: { x: 120, y: 120 },
     //     data: {
-    //         category: 'MAP',
-    //         keyType: { category: 'STRING' },
-    //         valueType: { category: 'INTEGER' },
+    //         data: {
+    //             category: 'STREAM',
+    //             elementType: { category: 'LIST', elementType: { category: 'STRING' } },
+    //         },
+    //         displayName: 'Arkivansvarlig',
+    //     },
+    // },
+    // {
+    //     id: '2',
+    //     type: 'variableNode',
+    //     position: { x: 120, y: 240 },
+    //     data: {
+    //         data: {
+    //             category: 'STREAM',
+    //             elementType: { category: 'LIST', elementType: { category: 'STRING' } },
+    //         },
+    //         displayName: 'Saksnummer',
+    //     },
+    // },
+    // {
+    //     id: '3',
+    //     type: 'variableNode',
+    //     position: { x: 120, y: 350 },
+    //     data: {
+    //         data: {
+    //             category: 'STREAM',
+    //             elementType: { category: 'LIST', elementType: { category: 'INTEGER' } },
+    //         },
+    //         displayName: 'SaksId',
+    //     },
+    // },
+    // {
+    //     id: '4',
+    //     type: 'variableNode',
+    //     position: { x: 120, y: 460 },
+    //     data: {
+    //         data: {
+    //             category: 'LIST',
+    //             elementType: { category: 'STRING' },
+    //         },
+    //         displayName: 'Person X',
+    //     },
+    // },
+    // {
+    //     id: '5',
+    //     type: 'variableNode',
+    //     position: { x: 120, y: 570 },
+    //     data: {
+    //         data: {
+    //             category: 'MAP',
+    //             keyType: { category: 'STRING' },
+    //             valueType: { category: 'INTEGER (Y)' },
+    //         },
+    //         displayName: 'Case Y',
     //     },
     // },
     {
-        id: '1',
-        type: 'variableNode',
-        position: { x: 120, y: 120 },
+        id: '6',
+        type: 'variableInlineNode',
+        position: { x: 120, y: 170 },
         data: {
-            data: {
-                category: 'STREAM',
-                elementType: { category: 'LIST', elementType: { category: 'STRING' } },
-            },
-            displayName: 'Arkivansvarlig',
+            category: 'STRING',
         },
     },
     {
-        id: '2',
-        type: 'variableNode',
-        position: { x: 120, y: 240 },
+        id: '7',
+        type: 'variableInlineNode',
+        position: { x: 120, y: 290 },
         data: {
-            data: {
-                category: 'STREAM',
-                elementType: { category: 'LIST', elementType: { category: 'STRING' } },
-            },
-            displayName: 'Saksnummer',
+            category: 'INTEGER',
         },
     },
     {
-        id: '3',
-        type: 'variableNode',
-        position: { x: 120, y: 350 },
+        id: '8',
+        type: 'variableInlineNode',
+        position: { x: 120, y: 400 },
         data: {
-            data: {
-                category: 'STREAM',
-                elementType: { category: 'LIST', elementType: { category: 'INTEGER' } },
-            },
-            displayName: 'SaksId',
-        },
-    },
-    {
-        id: '4',
-        type: 'variableNode',
-        position: { x: 120, y: 460 },
-        data: {
-            data: {
-                category: 'LIST',
-                elementType: { category: 'STRING' },
-            },
-            displayName: 'Person X',
-        },
-    },
-    {
-        id: '5',
-        type: 'variableNode',
-        position: { x: 120, y: 570 },
-        data: {
-            data: {
-                category: 'MAP',
-                keyType: { category: 'STRING' },
-                valueType: { category: 'INTEGER (Y)' },
-            },
-            displayName: 'Case Y',
+            category: 'DECIMAL',
         },
     },
 ] as Node[];
--- app/routes/integrations/nodes/VariableInlineNode.tsx
+++ app/routes/integrations/nodes/VariableInlineNode.tsx
@@ -1,16 +1,86 @@
+import { TextField } from '@navikt/ds-react';
 import { DataType } from '~/types/types';
 
-interface VariableInlineNodeProps {
+interface NodeData {
     data: DataType;
+    displayName: string;
 }
 
-const VariableInlineNode: React.FC<VariableInlineNodeProps> = ({ data }) => {
+// interface VariableInlineNodeProps {
+//     data: DataType;
+// }
+
+function getIcon(inputType: string) {
+    switch (inputType) {
+        case 'STRING':
+            return 'text_fields';
+        case 'INTEGER':
+            return 'numbers';
+        case 'DECIMAL':
+            return 'decimal_increase';
+        case 'time':
+            return 'schedule';
+        case 'date':
+            return 'calendar_today';
+        case 'datetime':
+            return 'calendar_clock';
+        case 'boolean':
+            return 'toggle_on';
+        default:
+            return 'text_fields';
+    }
+}
+
+function getTextFieldType(category: string) {
+    switch (category) {
+        case 'STRING':
+            return 'text';
+        case 'INTEGER':
+            return 'number';
+        default:
+            return 'text';
+    }
+}
+
+const VariableInlineNode: React.FC<NodeData> = ({ data }) => {
+    console.log(data);
+
     return (
         <>
             <p className="text-sm">Variable Inline Node</p>
-            <div className="p-4 border rounded border-gray-400 flex flex-row gap-2">
-                <label className="p-2 border rounded border-gray-400 ">{data.category}</label>
-                {/* display the correct input field based on data type */}
+            <div className="bg-blue-200 p-2 rounded border border-gray-400">
+                <div className=" flex flex-row gap-2">
+                    <div className="flex">
+                        <div className="flex">
+                            <div
+                                className={`
+                    flex p-[5px] border border-gray-400 
+                    rounded-tr-md rounded-br-md 
+                    bg-white                `}>
+                                <label className="bg-purple-200 p-3">{data.category}</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center p-1 bg-white border rounded border-gray-400">
+                        <div className="w-6 h-4 justify-center items-center gap-2.5 flex">
+                            <div className="w-6 h-7 relative">
+                                <span className="material-symbols-outlined mr-1">
+                                    {getIcon(data.category)}
+                                </span>
+                            </div>
+                        </div>
+                        <TextField
+                            type={getTextFieldType(data.category)}
+                            className="mr-2"
+                            label={''}
+                            hideLabel
+                            size="small"
+                            title="Please enter a decimal number"
+                            onChange={(e) => {}}
+                        />
+                    </div>
+                    {/* Input field based on category type, primitive og klokkeslett */}
+                </div>
             </div>
         </>
     );
