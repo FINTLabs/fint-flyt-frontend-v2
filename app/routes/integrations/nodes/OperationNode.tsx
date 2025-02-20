3ef0d02c5816feac4ca1bc1968dfe40e941aa6ec
Anum Qudsia <hello@anum.io>
2025-02-20 10:20:16 +0100

    Clean up in OperationNode

--- app/routes/integrations/nodes/OperationNode.tsx
+++ app/routes/integrations/nodes/OperationNode.tsx
@@ -1,8 +1,6 @@
 import { ChevronRightDoubleCircleFillIcon } from '@navikt/aksel-icons';
 import React from 'react';
-import { Connection, Position } from 'reactflow';
-import nodeConfig from '~/routes/integrations/nodes/config';
-import CustomHandle from './CustomHandle';
+import { Connection } from 'reactflow';
 import { OperationDeclaration } from '../types/Operation';
 import { BaseNode } from './BaseNode';
 
@@ -13,28 +11,9 @@ interface Props {
     data: OperationDeclaration;
 }
 
-const OperationNode: React.FC<Props> = ({ data, type, selected }) => {
-    // console.log('OperationNode: ', data);
-
-    const renderIcon = () => {
-        if (data.iconId === 'ChevronRightDoubleCircleFillIcon') {
-            return <ChevronRightDoubleCircleFillIcon title="a11y-title" fontSize="40px" />;
-        } else {
-            return (
-                <span className="material-symbols-outlined text-left text-[40px]">
-                    {data.iconId}
-                </span>
-            );
-        }
-    };
-
-    const backgroundColor = 'bg-sky-200';
+const OperationNode: React.FC<Props> = ({ data, selected }) => {
     const leftHandles = data.operationVariables.inputVariables.sort((a, b) => a.order - b.order);
     const rightHandles = data.operationVariables.outputVariables.sort((a, b) => a.order - b.order);
-    const maxHandles =
-        leftHandles.length > rightHandles.length ? leftHandles.length : rightHandles.length;
-
-    const dynamicHeight = `${maxHandles > 2 ? maxHandles * 2 : 5}rem`;
 
     const isValidConnection = (connection: Connection) => {
         console.log('Connection Attempt:', connection);
